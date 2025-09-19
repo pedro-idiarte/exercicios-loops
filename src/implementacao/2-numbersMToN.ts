74c65ea59e5b013ecdb1e5de05dcea3ff69aa0cc
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-19 11:19:37 -0300

    exercicio m to n resolvido

--- src/implementacao/2-numbersMToN.ts
+++ src/implementacao/2-numbersMToN.ts
@@ -1,3 +1,14 @@
 export function showNumbersBetween(M: number, N: number): number[] {
-    return [0];
+    
+    const numeros: number[] = [];
+    
+    if (M < N) {
+        for (let i = M; i <= N; i++){
+            console.log(i);
+            numeros.push(i);
+        }
+        return numeros;
+    }else{
+        throw new Error("M deve ser menor que N");
+    }   
 }
