d964f041873524981ef653883d3e784124a441ee
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-19 11:47:41 -0300

    exercicio analyze multiples resolvido

--- src/implementacao/4-analyzeMultiples.ts
+++ src/implementacao/4-analyzeMultiples.ts
@@ -2,6 +2,18 @@ export function analyzeMultiples (numeros: number[]) {
   const multiplos2e5: number[] = [];
   const multiplos2e3: number[] = [];
 
+  for (const numero of numeros) {
+    if (numero === -1) {
+      break;
+    }
+    if (numero % 2 === 0 && numero % 5 === 0) {
+      multiplos2e5.push(numero);
+    }
+    if (numero % 2 === 0 && numero % 3 === 0) {
+      multiplos2e3.push(numero);
+    }
+  }
+
   return { multiplos2e5, multiplos2e3 };
 }
 
