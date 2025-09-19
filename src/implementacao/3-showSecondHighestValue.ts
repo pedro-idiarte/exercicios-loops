e4e671832bf2c174ae4d19db0e7f2ff9eadec3d1
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-19 11:43:42 -0300

    exercicio second highest number resolvido

--- src/implementacao/3-showSecondHighestValue.ts
+++ src/implementacao/3-showSecondHighestValue.ts
@@ -1,3 +1,34 @@
 export function showSecondHighestValue(numbers: number[]): number {
-    return 0;
+
+    if (numbers.length < 2) {
+        throw new Error("O array deve conter pelo menos dois números.");
+    }
+
+    let highest = numbers[0];
+    let secondHighest = numbers[0];
+
+    for(const num of numbers) {
+        if(num>highest){
+            highest = num
+        }
+    }
+
+    let foundSecond = false;
+
+    for(const num of numbers) {
+        if(num<highest && num>secondHighest){
+            secondHighest = num
+            foundSecond = true;
+        }if(num<highest && secondHighest === highest){
+            secondHighest = num
+            foundSecond = true;
+        }
+    }
+
+    if (!foundSecond) {
+        throw new Error("Não foi possível encontrar um segundo numero.");
+    }
+    
+    return secondHighest;
+    
 }
