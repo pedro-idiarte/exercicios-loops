0a67a67fc3df044d07d2f02ed8a7d69a23439072
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-19 10:42:44 -0300

    exercicio numbers to N resolvido

--- src/implementacao/1-showNumbersToN.ts
+++ src/implementacao/1-showNumbersToN.ts
@@ -1,4 +1,10 @@
 export function showNumbersToN(N: number): number[] {
     const numeros: number[] = [];
+    
+    for (let i = 0; i <= N; i++){
+        console.log(i);
+        numeros.push(i);
+        }
+
     return numeros;
 }
