a2d461b16b4aaca700151dd0055eb78128a143e0
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-19 11:57:04 -0300

    exercicio average resolvido

--- src/implementacao/5-average.ts
+++ src/implementacao/5-average.ts
@@ -3,5 +3,27 @@ export function average(
   aluno2: number[],
   aluno3: number[]
 ): string[] {
-  return [""];
+
+  const resultados: string[] = [];
+
+  const alunos = [aluno1, aluno2, aluno3]; 
+  
+  for (let i = 0; i < alunos.length; i++) {
+    const notas = alunos[i];
+    let soma = 0;
+    for (let j = 0; j < notas.length; j++) {
+      soma += notas[j];
+    }
+    const media = soma / notas.length;
+    if (media >= 7) {
+      resultados.push(`Aluno ${i + 1}: Passou`);
+    }
+    else if (media > 4 && media < 7) {
+      resultados.push(`Aluno ${i + 1}: Recuperação`);
+    } else {
+      resultados.push(`Aluno ${i + 1}: Reprovado`);
+    }
+  }
+
+  return resultados;
 }
